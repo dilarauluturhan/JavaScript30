ac1bb0b16fdd3621d66b5209dbc3da5bbdbd96e5
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-05-08 12:48:07 +0300

    Day 29 added

---
+++ 29-countdown-clock/script.js
@@ -0,0 +1,55 @@
+let countdown;
+const timerDisplay = document.querySelector('.display_time-left');
+const endTime = document.querySelector('.display_end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) {
+    // mevcut zamanlayıcıları temizle
+    clearInterval(countdown);
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // durdurmamız gerekip gerekmediğini kontrol et
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        // göster
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
